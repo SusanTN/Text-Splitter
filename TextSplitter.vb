fc6875169ae915f59740a03ef0482f937ae573c8
SusanTN <susan@susans.org>
2023-11-27 18:38:43 -0600

    Initial Upload
    Introducing the Text Splitter:
    
    The Text Splitter is a user-friendly and versatile Windows-based desktop application designed to simplify the process of breaking down lengthy text into smaller, manageable segments. Whether you're preparing content for social media or any text-based platform with character limits, this app has you covered. It offers an intuitive interface that allows you to input your text, set segment parameters, and include optional footer text.
    
    Key Features:
    
        Effortless Text Input: Just paste or type your text into the left column, including any URLs. The app supports multi-line text for convenience.
    
        Smart Segmentation: Specify the maximum character limit for each segment, and let the app intelligently split your text, ensuring sentences remain intact.
    
        Footer Customization: Tailor your segments with footer text. This feature is ideal for adding hashtags, references, or consistent text to each segment.
    
        Include Footer in All Segments: Choose whether to include the footer text in every segment or only in the first one, offering flexibility in your content creation.
    
        Segment Count: Keep track of your text with the total character count and the number of segments generated based on your settings.
    
        Clear Output Display: The segmented text appears in the right column, neatly separated by line breaks. Footer text is clearly indicated, making it easy to use in various contexts.
    
    Requirements:
    
    To run the Text Splitter application, ensure that you meet the following requirements:
    
        Operating System: The Text Splitter is compatible with Windows-based systems, including Windows 7, 8, and 10. Please note that it may not work on other operating systems.
    
        Hardware: Minimal hardware requirements; it runs smoothly on most modern Windows-compatible computers. A standard keyboard and mouse are sufficient for interaction.
    
        Screen Resolution: For optimal display, a minimum screen resolution of 1024x768 is recommended.
    
        Software Dependencies: The Text Splitter is a standalone executable and doesn't rely on additional software dependencies or libraries.
    
        Accessibility Tools: The application is designed to be compatible with screen readers and supports keyboard navigation, ensuring accessibility for visually impaired users.
    
        User Knowledge: Users should have basic computer literacy and be familiar with text input and standard Windows interface conventions.
    
        Internet Connection: The Text Splitter operates locally on your computer and doesn't require an internet connection.
    
        Optional Clipboard Functionality: While not mandatory, clipboard functions can streamline the process of copying and pasting text into the application. Simply highlight the text and right-click. Alternatively, you can use text shortcuts such as Ctrl-C to copy and Ctrl-V to paste.
    
    Installation:
    
    Getting started with the Text Splitter is hassle-free. It's a standalone executable file, so simply download and run it on your Windows computer. No complex installation or setup process is required.
    
    Usage:
    
    With the Text Splitter, you can effortlessly input text, configure segmentation settings, and split your text within the user-friendly interface. The resulting segmented text can be easily copied and pasted into other applications or platforms as needed.
    
    Empower your content creation with the Text Splitter—a practical tool for breaking down extensive text into digestible segments. Whether you're a professional or creative writer, this application simplifies your workflow and enhances your productivity.

---
+++ TextSplitter.vb
@@ -0,0 +1,228 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms
+Imports System.Drawing
+
+Public Class TextSplitter
+	Inherits Form
+	Private WithEvents TextInput As TextBox
+	Private WithEvents FooterText As TextBox
+	Private WithEvents SplitText As Button
+	Private WithEvents MaxCharacters As NumericUpDown
+	Private WithEvents IncludeFooterInAllSegmentsCheckbox As CheckBox
+	Private FooterTextLabel As Label
+	Private maxCharactersLabel As Label
+	Private totalCharactersLabel As Label
+	Private numberOfSegmentsLabel As Label
+	Private TextOutput As TextBox
+
+	Public Sub New()
+		InitializeComponent()
+		CustomInitializeComponent()
+	End Sub
+	Protected Overrides Sub OnLoad(e As EventArgs)
+		MyBase.OnLoad(e)
+		AddHandler Me.Resize, AddressOf TextSplitter_Resize
+	End Sub
+	Private Sub CustomInitializeComponent()
+		TextInput = New TextBox With {
+			.Multiline = True,
+			.Location = New Point(10, 10),
+			.Size = New Size(300, 575),
+			.MinimumSize = New Size(300, 575),
+			.ScrollBars = ScrollBars.Vertical
+		}
+		TextOutput = New TextBox With {
+			.Multiline = True,
+			.Location = New Point(320, 10),
+			.Size = New Size(300, 760),
+			.MinimumSize = New Size(300, 760),
+			.ScrollBars = ScrollBars.Vertical
+		}
+		FooterTextLabel = New Label With {
+			.Text = "Footer Text",
+			.Location = New Point(10, 120),
+			.Size = New Size(100, 20)
+		}
+
+		FooterText = New TextBox With {
+			.Location = New Point(120, 120),
+			.Size = New Size(190, 20)
+		}
+
+		maxCharactersLabel = New Label With {
+			.Text = "Max Characters",
+			.Location = New Point(10, 150),
+			.Size = New Size(100, 20)
+		}
+
+		MaxCharacters = New NumericUpDown With {
+			.Location = New Point(120, 150),
+			.Size = New Size(100, 20),
+			.Minimum = 1,
+			.Maximum = 1000,
+			.Value = 500
+		}
+
+		SplitText = New Button With {
+			.Text = "Split the text",
+			.Location = New Point(10, 180),
+			.Size = New Size(300, 30)
+		}
+
+		totalCharactersLabel = New Label With {
+			.Text = "Total Characters: 0",
+			.Location = New Point(10, 220),
+			.Size = New Size(300, 20)
+		}
+
+		numberOfSegmentsLabel = New Label With {
+			.Text = "Number of Segments: 0",
+			.Location = New Point(10, 250),
+			.Size = New Size(300, 20)
+		}
+
+		IncludeFooterInAllSegmentsCheckbox = New CheckBox With {
+			.Text = "Include Footer in All Segments",
+			.Location = New Point(10, 280),
+			.Size = New Size(300, 20),
+			.Checked = False
+		}
+		Controls.Add(TextInput)
+		Controls.Add(FooterTextLabel)
+		Controls.Add(FooterText)
+		Controls.Add(IncludeFooterInAllSegmentsCheckbox)
+		Controls.Add(maxCharactersLabel)
+		Controls.Add(MaxCharacters)
+		Controls.Add(SplitText)
+		Controls.Add(totalCharactersLabel)
+		Controls.Add(numberOfSegmentsLabel)
+		Controls.Add(TextOutput)
+		Size = New Size(642, 820)
+		Text = "Text Splitter"
+	End Sub
+	Private Sub TextSplitter_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+		ApplyLayout() ' This will adjust the layout based on the current form size
+	End Sub
+	Private Sub ApplyLayout()
+		If TextInput IsNot Nothing AndAlso TextOutput IsNot Nothing Then
+			' Calculate available width and height, keeping a margin
+			Dim availableWidth As Integer = Me.ClientSize.Width - 20
+			Dim availableHeight As Integer = Me.ClientSize.Height - 60 ' Adjust as needed for margins and other controls
+
+			' Calculate the widths for the text boxes
+			Dim textBoxWidth As Integer = Math.Max(TextInput.MinimumSize.Width, CInt(availableWidth * 0.45))
+			Dim textBoxHeight As Integer = CInt((availableHeight - 120) * 0.5) ' Adjust as needed
+
+			' Set the width and height of the TextInput
+			TextInput.Width = textBoxWidth
+			TextInput.Height = textBoxHeight
+
+			' Position FooterTextLabel directly above FooterText
+			FooterTextLabel.Location = New Point(10, TextInput.Bottom + 10)
+			FooterTextLabel.Width = textBoxWidth
+
+			' Position FooterText directly below FooterTextLabel
+			FooterText.Location = New Point(10, FooterTextLabel.Bottom + 5)
+			FooterText.Width = textBoxWidth
+
+			' Position IncludeFooterInAllSegmentsCheckbox below FooterText
+			IncludeFooterInAllSegmentsCheckbox.Location = New Point(10, FooterText.Bottom + 10)
+			IncludeFooterInAllSegmentsCheckbox.Width = textBoxWidth
+
+			' Position maxCharactersLabel and MaxCharacters below FooterText
+			maxCharactersLabel.Location = New Point(10, IncludeFooterInAllSegmentsCheckbox.Bottom + 10)
+			MaxCharacters.Location = New Point(120, IncludeFooterInAllSegmentsCheckbox.Bottom + 10)
+			MaxCharacters.Width = textBoxWidth - 110
+
+			' Position SplitText button below MaxCharacters
+			SplitText.Location = New Point(10, MaxCharacters.Bottom + 10)
+			SplitText.Width = textBoxWidth
+
+			' Position totalCharactersLabel to the left below SplitText
+			totalCharactersLabel.Location = New Point(10, SplitText.Bottom + 10)
+			totalCharactersLabel.Width = textBoxWidth / 2 - 5
+
+			' Position numberOfSegmentsLabel to the right, next to totalCharactersLabel
+			numberOfSegmentsLabel.Location = New Point(totalCharactersLabel.Right + 10, SplitText.Bottom + 10)
+			numberOfSegmentsLabel.Width = textBoxWidth / 2 - 5
+
+			' Set the location, width, and height of the TextOutput
+			TextOutput.Location = New Point(TextInput.Right + 10, 10)
+			TextOutput.Width = availableWidth - textBoxWidth - 10
+			TextOutput.Height = textBoxHeight * 2 + 20 ' Adjust as needed
+		End If
+	End Sub
+	Private Sub SplitText_Click(sender As Object, e As EventArgs) Handles SplitText.Click
+		Dim inputText As String = TextInput.Text
+		Dim maxCharLimit As Integer = Convert.ToInt32(MaxCharacters.Value)
+		Dim footerTemplate As String = FooterText.Text & " ({0}/{1})"
+		Dim includeFooterInAllSegments As Boolean = IncludeFooterInAllSegmentsCheckbox.Checked
+
+		Dim segments As List(Of String) = SplitIntoSegments(inputText, maxCharLimit, footerTemplate, includeFooterInAllSegments)
+		TextOutput.Text = String.Join(Environment.NewLine, segments)
+
+		totalCharactersLabel.Text = $"Total Characters: {inputText.Length}"
+		numberOfSegmentsLabel.Text = $"Number of Segments: {segments.Count}"
+	End Sub
+	Private Function SplitIntoSegments(inputText As String, maxCharacters As Integer, footerText As String, includeFooterInAllSegments As Boolean) As List(Of String)
+		' Dictionary to hold URL tags and their corresponding URLs
+		Dim urlDictionary As New Dictionary(Of String, String)()
+		Dim urlTagCounter As Integer = 0
+
+		' Regex to find URLs
+		Dim urlPattern As String = "https?://\S+"
+		Dim regex As New Regex(urlPattern)
+
+		' Replace URLs with tags and store them in the dictionary
+		For Each match As Match In regex.Matches(inputText)
+			Dim urlTag As String = $"[URL{urlTagCounter}]"
+			urlDictionary.Add(urlTag, match.Value)
+			inputText = inputText.Replace(match.Value, urlTag)
+			urlTagCounter += 1
+		Next
+
+		Dim segments As New List(Of String)()
+		Dim segmentCounter As Integer = 1
+
+		While inputText.Length > 0
+			Dim segmentSize As Integer = maxCharacters
+			If segmentCounter = 1 OrElse includeFooterInAllSegments Then
+				segmentSize -= footerText.Length + 10 ' Adjust size for footer
+			End If
+
+			Dim segmentEnd As Integer = Math.Min(segmentSize, inputText.Length)
+			Dim segmentText As String = inputText.Substring(0, segmentEnd)
+
+			' Find the last sentence terminator to avoid splitting mid-sentence
+			Dim sentenceTerminators As Char() = {"."c, "!"c, "?"c, """"c, ChrW(&H201D)}
+			Dim lastSentenceEnd As Integer = segmentText.LastIndexOfAny(sentenceTerminators)
+			If lastSentenceEnd > -1 AndAlso lastSentenceEnd < segmentSize - 1 Then
+				segmentText = segmentText.Substring(0, lastSentenceEnd + 1)
+			End If
+
+			' Append the footer or segment count as required, with formatting
+			If segmentCounter = 1 OrElse includeFooterInAllSegments Then
+				Dim footer As String = String.Format(footerText, segmentCounter, "{totalSegments}")
+				segments.Add(segmentText.Trim() & Environment.NewLine & Environment.NewLine & "— " & footer & Environment.NewLine & Environment.NewLine)
+			Else
+				segments.Add(segmentText.Trim() & Environment.NewLine & Environment.NewLine & "— (" & segmentCounter.ToString() & "/{totalSegments})" & Environment.NewLine & Environment.NewLine)
+			End If
+
+			inputText = inputText.Substring(segmentText.Length).Trim()
+			segmentCounter += 1
+		End While
+
+		' Replace the URL tags with actual URLs in each segment
+		Dim totalSegments As Integer = segments.Count
+		For i As Integer = 0 To segments.Count - 1
+			For Each kvp As KeyValuePair(Of String, String) In urlDictionary
+				segments(i) = segments(i).Replace(kvp.Key, kvp.Value)
+			Next
+			segments(i) = segments(i).Replace("{totalSegments}", totalSegments.ToString())
+		Next
+
+		Return segments
+	End Function
+End Class
